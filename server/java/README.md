ca9836b3b65f76295ecbd089b5700eac933643f5
Adrienne Dreyfus <adreyfus@stripe.com>
2019-06-13 09:15:54 -0700

    Added Java server

---
+++ server/java/README.md
@@ -0,0 +1,10 @@
+# Running the Spark server
+
+Requires Maven
+
+```
+mvn package
+java -cp target/some-recipe-1.0.0-SNAPSHOT-jar-with-dependencies.jar com.stripe.recipe.Server
+```
+
+Go to http://localhost:4567/
---
+++ server/java/src/main/java/com/stripe/recipe/Server.java
@@ -0,0 +1,82 @@
+package com.stripe.recipe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import com.stripe.Stripe;
+import com.stripe.model.Event;
+import com.stripe.exception.*;
+import com.stripe.net.Webhook;
+
+public class Server {
+    private static Gson gson = new Gson();
+
+    static class PostBody {
+        @SerializedName("some_field")
+        String someField;
+
+        public String getSomeField() {
+            return someField;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stripe.apiKey = System.getenv("STRIPE_SECRET_KEY");
+
+        get("/", (request, response) -> {
+            response.type("application/json");
+
+            Map<String, Object> responseData = new HashMap<>();
+            responseData.put("some_key", "some_value");
+            return gson.toJson(responseData);
+        });
+
+        post("/post", (request, response) -> {
+            PostBody postBody = gson.fromJson(request.body(), PostBody.class);
+
+            response.type("application/json");
+            System.out.println(postBody.getSomeField());
+            if (postBody.getSomeField().equals("something")) {
+                return "{\"data\": \"something \"}";
+            } else {
+                return "{\"data\": \"nothing \"}";
+            }
+        });
+
+        post("/webhook", (request, response) -> {
+            System.out.println("Webhook");
+            String payload = request.body();
+            String sigHeader = request.headers("Stripe-Signature");
+            String endpointSecret = System.getenv("STRIPE_WEBHOOK_SECRET");
+
+            Event event = null;
+
+            try {
+                event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
+            } catch (SignatureVerificationException e) {
+                // Invalid signature
+                response.status(400);
+                return "";
+            }
+
+            switch (event.getType()) {
+            case "payment_intent.succeeded":
+                System.out.println("Received event");
+                break;
+            default:
+                // Unexpected event type
+                response.status(400);
+                return "";
+            }
+
+            response.status(200);
+            return "";
+        });
+    }
+}
