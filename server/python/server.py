68574ea512b3362b622d0abbe2a773f4c46f28a9
Adrienne Dreyfus <adreyfus@stripe.com>
2019-06-25 20:15:31 -0700

    Added sample python webhook

--- server/python/server.py
+++ server/python/server.py
@@ -34,5 +34,36 @@ def post_example():
     except Exception as e:
         return jsonify(e), 403
 
+@app.route('/webhook', methods=['POST'])
+def webhook_received():
+    # You can use webhooks to receive information about asynchronous payment events.
+    # For more about our webhook events check out https://stripe.com/docs/webhooks.
+    webhook_secret = os.getenv('STRIPE_WEBHOOK_SECRET')
+    request_data = json.loads(request.data)
+
+    if webhook_secret:
+        # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+        signature = request.headers.get('stripe-signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload=request.data, sig_header=signature, secret=webhook_secret)
+            data = event['data']
+        except Exception as e:
+            return e
+        # Get the type of webhook event sent - used to check the status of PaymentIntents.
+        event_type = event['type']
+    else:
+        data = request_data['data']
+        event_type = request_data['type']
+    data_object = data['object']
+    
+    print('event ' + event_type)
+
+    if event_type == 'some.event':
+        print('🔔Webhook received!')
+
+    return jsonify({'status': 'success'})
+
+
 if __name__== '__main__':
-    app.run()
+    app.run(port=4242)
