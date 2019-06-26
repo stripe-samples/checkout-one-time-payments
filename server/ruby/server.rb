929f3a485b285ecfd821b628d760e9904b262e7b
Adrienne Dreyfus <adreyfus@stripe.com>
2019-06-25 20:22:25 -0700

    Added webhook to ruby server

--- server/ruby/server.rb
+++ server/ruby/server.rb
@@ -2,11 +2,12 @@ require 'stripe'
 require 'sinatra'
 require 'dotenv'
 
-Dotenv.load(File.dirname(__FILE__) + '/../../.env.example')
+Dotenv.load(File.dirname(__FILE__) + '/../../.env')
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
 set :static, true
 set :public_folder, File.join(File.dirname(__FILE__), '../../client/')
+set :port, 4242
 
 get '/' do
   content_type 'text/html'
@@ -20,4 +21,48 @@ post '/' do
   {
     data: data
   }.to_json
+end
+
+post '/webhook' do
+  # You can use webhooks to receive information about asynchronous payment events.
+  # For more about our webhook events check out https://stripe.com/docs/webhooks.
+  webhook_secret = ENV['STRIPE_WEBHOOK_SECRET']
+  payload = request.body.read
+  if !webhook_secret.empty?
+    # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
+  
+    begin
+      event = Stripe::Webhook.construct_event(
+          payload, sig_header, webhook_secret
+      )
+    rescue JSON::ParserError => e
+        # Invalid payload
+        status 400
+        return
+    rescue Stripe::SignatureVerificationError => e
+        # Invalid signature
+        puts "⚠️  Webhook signature verification failed."
+        status 400
+        return
+    end
+  else
+    data = JSON.parse(payload, symbolize_names: true)
+    event = Stripe::Event.construct_from(data)
+  end
+  # Get the type of webhook event sent - used to check the status of PaymentIntents.    
+  event_type = event['type']
+  data = event['data']
+  data_object = data['object']
+
+  if event_type == 'some.event'
+    puts "🔔  Webhook received!"
+  end
+
+  content_type 'application/json'
+  {
+    status: 'success'
+  }.to_json
+
 end
