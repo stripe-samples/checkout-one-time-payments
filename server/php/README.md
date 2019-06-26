49070aa5716d53a0c0f281de93ead652cfe1a479
Adrienne Dreyfus <adreyfus@stripe.com>
2019-06-25 20:42:39 -0700

    Added Slim server

---
+++ server/php/README.md
@@ -0,0 +1,19 @@
+# PHP example
+
+## Requirements
+* PHP >= 7.1.3
+* [Slim](http://www.slimframework.com/)
+
+## How to run
+
+1. Run composer to set up dependencies
+
+```
+php composer.json install
+```
+
+2. Run the server locally
+
+```
+php -S localhost:8080
+```
---
+++ server/php/index.php
@@ -0,0 +1,67 @@
+<?php
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Stripe\Stripe;
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::create(realpath('../..'));
+$dotenv->load();
+
+$app = new \Slim\App;
+
+// Instantiate the logger as a dependency
+$container = $app->getContainer();
+$container['logger'] = function ($c) {
+  $settings = $c->get('settings')['logger'];
+  $logger = new Monolog\Logger($settings['name']);
+  $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+  $logger->pushHandler(new Monolog\Handler\StreamHandler(__DIR__ . '/logs/app.log', \Monolog\Logger::DEBUG));
+  return $logger;
+};
+
+$app->add(function ($request, $response, $next) {
+    Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
+    return $next($request, $response);
+});
+  
+
+$app->get('/', function (Request $request, Response $response, array $args) {   
+    return $response->write(file_get_contents('../../client/index.html'));
+
+});
+
+$app->get('/public-key', function (Request $request, Response $response, array $args) {
+    $pub_key = getenv('STRIPE_PUBLIC_KEY');
+
+    $response->getBody()->write("Hello, $pub_key");
+    return $response;
+});
+
+$app->post('/webhook', function(Request $request, Response $response) {
+    $logger = $this->get('logger');
+    $event = $request->getParsedBody();
+    // Parse the message body (and check the signature if possible)
+    $webhookSecret = getenv('STRIPE_WEBHOOK_SECRET');
+    if ($webhookSecret) {
+      try {
+        $event = \Stripe\Webhook::constructEvent(
+          $request->getBody(),
+          $request->getHeaderLine('stripe-signature'),
+          $webhookSecret
+        );
+      } catch (\Exception $e) {
+        return $response->withJson([ 'error' => $e->getMessage() ])->withStatus(403);
+      }
+    } else {
+      $event = $request->getParsedBody();
+    }
+    $type = $event['type'];
+    $object = $event['data']['object'];
+  
+    $logger->info('🔔  Webhook received! ' . $type);
+
+    return $response->withJson([ 'status' => 'success' ])->withStatus(200);
+});
+
+$app->run();
