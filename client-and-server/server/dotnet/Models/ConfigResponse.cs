260ea7c94c62deb340314cc6ccdf8a9df0eafd23
CJ Avilla <cjavilla@stripe.com>
2020-09-04 15:02:59 -0700

    Adding dotnet server

---
+++ client-and-server/server/dotnet/Models/ConfigResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+public class ConfigResponse
+{
+    [JsonProperty("publicKey")]
+    public string PublicKey { get; set; }
+
+    [JsonProperty("unitAmount")]
+    public long? UnitAmount { get; set; }
+
+    [JsonProperty("currency")]
+    public string Currency { get; set; }
+}
---
+++ client-and-server/server/dotnet/Models/CreateCheckoutSessionResponse.cs
@@ -0,0 +1,7 @@
+﻿using Newtonsoft.Json;
+
+public class CreateCheckoutSessionResponse
+{
+    [JsonProperty("sessionId")]
+    public string SessionId { get; set; }
+}
