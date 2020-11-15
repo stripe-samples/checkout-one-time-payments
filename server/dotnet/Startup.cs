954ed298355be5c2ce912477d9da39594fc3b655
David Martin Vela <david.mv@nuvolar.eu>
2020-11-15 13:23:16 +0100

    Fix Typo

--- server/dotnet/Startup.cs
+++ server/dotnet/Startup.cs
@@ -36,7 +36,7 @@ namespace server
                 options.SecretKey = Environment.GetEnvironmentVariable("STRIPE_SECRET_KEY");
                 options.WebhookSecret = Environment.GetEnvironmentVariable("STRIPE_WEBHOOK_SECRET");
                 options.Price = Environment.GetEnvironmentVariable("PRICE");
-                options.PaymentMethodTypes = Environment.GetEnvironmentVariable("PAYMENT_METHODS");
+                options.PaymentMethodTypes = Environment.GetEnvironmentVariable("PAYMENT_METHOD_TYPES");
                 options.Domain = Environment.GetEnvironmentVariable("DOMAIN");
             });
 
