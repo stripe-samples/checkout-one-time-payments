d57e8e29096555976e51caff356b30861b78b4e3
Catherine Moresco <cmoresco@stripe.com>
2026-04-01 15:30:51 -0400

    fix whitespace

--- server/python/server.py
+++ server/python/server.py
@@ -21,8 +21,9 @@ stripe.set_app_info(
     url='https://github.com/stripe-samples/checkout-one-time-payments')
 
 stripe.api_version = '2020-08-27'
-# Never put any keys in code! Use a secrets vault or environment
-# variable to supply keys to your integration. 
+# Never put any keys in code! Always use a secrets vault or environment
+# variable to supply keys to your integration.
+#
 # See https://docs.stripe.com/keys-best-practices and find your
 # keys at https://dashboard.stripe.com/apikeys.
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
