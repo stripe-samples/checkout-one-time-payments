6589fdcf7d60f6937a78632556a98aa4e600a5a0
Kate Huang <katejh@stripe.com>
2026-06-03 16:25:12 -0400

    unpin old API versions

--- server/python/server.py
+++ server/python/server.py
@@ -20,7 +20,6 @@ stripe.set_app_info(
     version='0.0.1',
     url='https://github.com/stripe-samples/checkout-one-time-payments')
 
-stripe.api_version = '2020-08-27'
 # Don't put any keys in code. Use an environment variable (as shown
 # here) or secrets vault to supply keys to your integration.
 #
